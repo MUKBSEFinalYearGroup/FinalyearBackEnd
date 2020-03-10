2d1250f4f491134c19bf07c49edea934544643ed
microsoftjulius <julisema4@gmail.com>
2020-03-10 07:03:29 -0400

    Test methods created

---
+++ tests/Feature/packagesTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class packagesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function createPackage(){
+        
+    }
+
+    /** @test */
+    public function editPackage(){
+
+    }
+
+    /** @test */
+    public function assignPaymentToPackage(){
+
+    }
+
+    /** @test */
+    public function getAllPackages(){
+
+    }
+
+    /** @test */
+    public function deletePackageTemporarily(){
+
+    }
+
+    /** @test */
+    public function deletePackageParmanetly(){
+
+    }
+}
